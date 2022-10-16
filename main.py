6b8277da0ea56f7d5f1cddc2ae28fcf8fa7c155b
LuShengRui <110027094+LuShengRui@users.noreply.github.com>
2022-10-16 19:58:53 +0800

    Add files via upload

---
+++ main.py
@@ -0,0 +1,22 @@
+import siot
+import os
+
+
+siot.init(client_id="",server="54q91g0678.goho.co",port=11480,user="CloudUser",password="CloudUser")
+siot.connect()
+vscooddir = dir /s
+my_variable = cmd
+131
+siot.publish(topic="topic", data="esc")
+
+while True:
+    print(my_variable)
+    print(vscooddir)
+    if ((vscooddir == dir /s) and (my_variable == cmd)):
+        pass
+    else:
+        vscooddir = dir /s
+        my_variable = cmd
+    if (s >= s2):
+        os.system(cmd)
+        os.system(dir /s)
